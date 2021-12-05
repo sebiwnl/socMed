2c454227df8f890b59c449f29d859837781b74ac
(your name) <(your mail address)>
2021-12-05 18:14:15 +0100

    14. google sigIn

--- pages/enter.js
+++ pages/enter.js
@@ -1,9 +1,37 @@
 import { Main } from "next/document";
+import { auth, googleAuthProvider} from '../lib/firebase';
 
 export default function EnterPage({ }) {
+    const user = null;
+    const username = null;
+
     return (
         <main>
-                <h1>Sign Up my man</h1>
+                {user ?
+                    !username ? <UsernameForm /> : <SignOutButton />
+                    :
+                    <SignInButton />
+                }
         </main>
     )
+}
+
+function SignInButton() {
+    const singInWithGoogle = async () => {
+        await auth.signInWithPopup(googleAuthProvider);
+    }
+
+    return (
+        <button className="btn-google" onClick={singInWithGoogle}>
+            <img src={'google.png'} /> sign in with Google
+        </button>
+    )
+}
+
+function SignOutButton() {
+    return <button onClick={() => auth.signOut()}>Sign Out</button>
+}
+
+function UsernameForm() {
+    return null;
 }
